c7593a8b63f334cfec92a82b4fe67a9689e3eb7c
ntt65 <ntt6551@gmail.com>
2026-04-15 22:23:14 +0900

    Feat:model.py mac_simulation()

--- e1_3/src/model.py
+++ e1_3/src/model.py
@@ -1,6 +1,14 @@
 class Model:
     def __init__(self):
-        self.data = {
-            "quizzes": [],  # 퀴즈 문제와 선택지 저장
-            "records": []   # 게임 기록 저장
-        }
+        self.filter_a = None
+        self.filter_b = None
+        self.pattern = None
+    
+    def mac_simulation(self, filter, pattern, iterations):
+        # MAC 시뮬레이션 로직
+        result = 0
+        for _ in range(iterations):
+            for i in range(len(pattern)):
+                for j in range(len(pattern[0])  if pattern else 0):
+                    result += filter[i][j] * pattern[i][j]
+        return result
